95cadcb35f757a2acb2298c780789f2789ff64a3
karlis87 <97662038+karlis87@users.noreply.github.com>
2022-02-24 14:52:37 +0200

    LAUKUMS

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function nolasa(m1,m2,m3){
         return false;
     }
 }
+function perimetrs(m1,m2,m3){
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1,m2,m3);
+    const p = m1 + m2 + m3;
+    console.log(p)
+    return (p);
+}
+function pusper(m1,m2,m3){
+    const pusper = perimetrs(m1,m2,m3)/2;
+    console.log(pusper);
+    return(pusper);
+}
+function laukums(m1,m2,m3){
+const pusper = perimetrs(m1,m2,m3)/2;
+let tlaukums = Math.sqrt(pusper * ((pusper - m1) * (pusper - m2) * (pusper-m3)));
+console.log(tlaukums);
+return tlaukums;
+}
+if  (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2){
+    return true;
+} else {
+    return false;
+
+}
